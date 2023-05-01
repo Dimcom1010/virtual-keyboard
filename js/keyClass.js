aa161ec5a28bc79c8b650f3728954b877712d46c
Dimcom1010 <dimcom1010@gmail.com>
2023-05-02 00:51:04 +0300

    deploy keyboard

---
+++ js/keyClass.js
@@ -0,0 +1,24 @@
+
+import { onPressAction } from './onPressAction.js'
+
+export function createKey(key) {
+    const keyboard = document.querySelector('#keyboard');
+    const keyElement = document.createElement('button');
+    keyElement.className = "key";
+    keyElement.dataset.code = `${key.keyCode}`;
+    keyElement.style.gridColumn = `${key.width}`;
+    keyElement.innerHTML = `${key.key}`;
+
+    const subkey = document.createElement('div');
+    subkey.className = "key__subkey";
+    subkey.innerHTML = `${key.subkey}`;
+    keyElement.appendChild(subkey);
+
+    _addEvent(keyElement, key.key);
+    keyboard.append(keyElement);
+}
+
+// add keys EventListener
+const _addEvent = (keyElement, value) => {
+    keyElement.addEventListener("click", () => onPressAction(value))
+}
